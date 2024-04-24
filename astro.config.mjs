1bd6ea11fa4a284ed4c29aae919a0d751844ab12
Tejas Sanap <email@whereistejas.xyz>
2024-04-24 13:46:43 +0100

    analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import vercel from "@astrojs/vercel/static";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  adapter: vercel(),
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
 });
