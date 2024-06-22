b5fc7f0e9184c9289ec1be29e0574e03e4d4ea6a
Tejas Sanap <email@whereistejas.xyz>
2024-06-23 00:56:22 +0100

    finish v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
   adapter: vercel({
     webAnalytics: { enabled: true }
   }),
