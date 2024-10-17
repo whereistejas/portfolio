97a2fc4b294137b01b7e182e932249096ee03d56
Tejas Sanap <email@whereistejas.xyz>
2024-10-17 09:49:11 +0100

    use vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,7 @@ import { defineConfig } from "astro/config";
 
 import vercel from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
 export default defineConfig({
-  // node({
-  //   mode: "standalone",
-  // }),
   output: "server",
-
   adapter: vercel(),
 });
