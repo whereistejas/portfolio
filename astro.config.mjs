3f5926374859af16ed4dd28973e286043209e305
Tejas Sanap <email@whereistejas.xyz>
2025-02-12 16:20:48 +0000

    Update astro versions and add `mdx` support.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 
 import vercel from "@astrojs/vercel/static";
+import mdx from "@astrojs/mdx";
 
 export default defineConfig({
   output: "static",
   adapter: vercel(),
+  integrations: [mdx()],
 });
