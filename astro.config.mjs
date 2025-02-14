0f4a1a9555a75c84c6ffe314f399cbde4b0e59e2
Tejas Sanap <email@whereistejas.xyz>
2025-02-14 10:41:53 +0000

    Add new pagecenter page.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel";
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
