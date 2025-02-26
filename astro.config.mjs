8c0f7d1bebd4f96d8211271fb33c3d847c2cac99
Tejas Sanap <email@whereistejas.xyz>
2025-02-26 22:31:09 +0000

    Add tailwind.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from "astro/config";
 
-import vercel from "@astrojs/vercel";
 import mdx from "@astrojs/mdx";
+import vercel from "@astrojs/vercel";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   output: "static",
   adapter: vercel(),
   integrations: [mdx()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
