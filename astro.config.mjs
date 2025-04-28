aa702a41c8c6ae5d757aac8cd5d1d7c08d43bd43
Tejas Sanap <email@whereistejas.xyz>
2025-04-28 13:46:37 +0100

    mew mew

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,11 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
-import { remarkWikilinks } from "./src/plugins/remarkWikilinks.mjs";
 
 export default defineConfig({
   output: "static",
   adapter: vercel(),
-  integrations: [
-    mdx({
-      remarkPlugins: [remarkWikilinks],
-    }),
-  ],
+  integrations: [mdx()],
   vite: {
     plugins: [tailwindcss()],
   },
