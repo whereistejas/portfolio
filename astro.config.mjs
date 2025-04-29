2aa461519da5091d53f85d6f02b51da0330835d4
Tejas Sanap <email@whereistejas.xyz>
2025-04-29 11:08:20 +0100

    new build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from "astro/config";
 
-import mdx from "@astrojs/mdx";
 import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   output: "static",
   adapter: vercel(),
-  integrations: [mdx()],
   vite: {
     plugins: [tailwindcss()],
   },
--- src/pages/archive/Books I recommend.md
+++ src/pages/archive/Books I recommend.md
@@ -3,6 +3,7 @@ date: 2025-02-13
 layout: ../../layouts/blog.astro
 title: Books I recommend
 ---
+
 Whenever someone asks me for a book recommendation, I always offer something from this list. I thought it would be really nice to make my first blog post about the books I love.
 
 - Samarkand
