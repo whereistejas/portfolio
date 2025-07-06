a2f348a826f5d23b9c7d7a26326c3bfe7e623a45
Tejas Sanap <email@whereistejas.xyz>
2025-07-06 20:18:19 +0100

    .github

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig  } from "astro/config";
+import { defineConfig } from "astro/config";
 
 import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  site: "https://whereistejas.xyz",
+  base: "",
   output: "static",
   adapter: vercel(),
   markdown: {
