3a1bdb31d0b8f174ebbef0f3cdbd816c75ffad8a
Tejas Sanap <email@whereistejas.xyz>
2025-09-08 22:11:51 +0100

    `prettier`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,26 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config'
 
-import tailwindcss from "@tailwindcss/vite";
-import remarkMath from "remark-math";
-import rehypeKatex from "rehype-katex";
+import tailwindcss from '@tailwindcss/vite'
+import remarkMath from 'remark-math'
+import rehypeKatex from 'rehype-katex'
 
 export default defineConfig({
-  site: "https://whereistejas.xyz",
-  base: "",
-  output: "static",
-  markdown: {
-    remarkRehype: {
-      footnoteLabel: " ",
-      footnoteLabelTagName: "hr",
-      footnoteLabelProperties: { className: [""] },
-    },
-    remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeKatex],
-  },
-  vite: {
-    plugins: [tailwindcss()],
-  },
-  experimental: {
-    headingIdCompat: true,
-  },
-});
+	site: 'https://whereistejas.xyz',
+	base: '',
+	output: 'static',
+	markdown: {
+		remarkRehype: {
+			footnoteLabel: ' ',
+			footnoteLabelTagName: 'hr',
+			footnoteLabelProperties: { className: [''] },
+		},
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypeKatex],
+	},
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	experimental: {
+		headingIdCompat: true,
+	},
+})
